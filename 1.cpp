26d01114084612fa9d1ab5359022cfdd025096d1
Sushant <sushant13042002@gmail.com>
2022-10-10 22:54:17 +0530

    added for loop to make half prism

--- 1.cpp
+++ 1.cpp
@@ -4,6 +4,20 @@ using namespace std;
 int main()
 {
     cout<<"Hello Silent Chora";
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for(int i = 1; i <= rows; ++i)
+    {
+        for(int j = 1; j <= i; ++j)
+        {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    
     
     return 0;
 }
